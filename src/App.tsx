2952b2564f52ce435e98c0d00cf39d01ac22084c
thanh.nguyen21 <thanh.nguyen21@mservice.com.vn>
2022-04-23 15:18:37 +0700

    fetch data from heroku

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('https://bear-bun.herokuapp.com/WeatherForecast').then(res => res.json().then(json => console.log(json)))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
